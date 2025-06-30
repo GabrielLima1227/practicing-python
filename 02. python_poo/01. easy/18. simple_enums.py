072d7bcb88fb42dbc12629c5dd411a0e294388d0
Gabriel de Lima Sousa <gablimax@hotmail.com>
2025-06-30 10:04:29 -0300

    fix:Activity simple enums language

--- 02. python_poo/01. easy/18. simple_enums.py
+++ 02. python_poo/01. easy/18. simple_enums.py
@@ -1,44 +1,43 @@
-class StatusPedido:
-    PENDENTE = "Pendente"
-    PROCESSANDO = "Processando"
-    ENVIADO = "Enviado"
-    ENTREGUE = "Entregue"
-    CANCELADO = "Cancelado"
-    STATUS_VALIDOS = {PENDENTE, PROCESSANDO, ENVIADO, ENTREGUE, CANCELADO}
+class OrderStatus:
+    PENDING = "Pending"
+    PROCESSING = "Processing"
+    SHIPPED = "Shipped"
+    DELIVERED = "Delivered"
+    CANCELED = "Canceled"
+    VALID_STATUSES = {PENDING, PROCESSING, SHIPPED, DELIVERED, CANCELED}
 
-class Pedido:
-    def __init__(self, id: int):
-        self.id = id
-        self.status = StatusPedido.PENDENTE
-        print(f"Pedido {self.id} criado com status: {self.status}")
+class Order:
+    def __init__(self, order_id: int):
+        self.order_id = order_id
+        self.status = OrderStatus.PENDING
+        print(f"Order {self.order_id} created with status: {self.status}")
 
     def __str__(self):
-        return f"Pedido ID: {self.id}, Status Atual: {self.status}"
+        return f"Order ID: {self.order_id}, Current Status: {self.status}"
 
-    def alterar_status(self, novo_status: str) -> None:
-        if novo_status in StatusPedido.STATUS_VALIDOS:
-            self.status = novo_status
-            print(f"Status do Pedido {self.id} alterado para: {self.status}")
+    def change_status(self, new_status: str) -> None:
+        if new_status in OrderStatus.VALID_STATUSES:
+            self.status = new_status
+            print(f"Status of Order {self.order_id} changed to: {self.status}")
         else:
-            print(f"Erro: '{novo_status}' não é um status de pedido válido. "
-                f"Status válidos são: {', '.join(StatusPedido.STATUS_VALIDOS)}")
+            print(f"Error: '{new_status}' is not a valid order status. "
+                f"Valid statuses are: {', '.join(OrderStatus.VALID_STATUSES)}")
 
+order1 = Order(order_id=101)
+print(order1)
 
-pedido1 = Pedido(id=101)
-print(pedido1)
+order1.change_status(OrderStatus.PROCESSING)
+print(order1)
 
-pedido1.alterar_status(StatusPedido.PROCESSANDO)
-print(pedido1)
+order1.change_status(OrderStatus.SHIPPED)
+print(order1)
 
-pedido1.alterar_status(StatusPedido.ENVIADO)
-print(pedido1)
+order1.change_status("InTransit")
+print(order1)
 
-pedido1.alterar_status("EmTrânsito")
-print(pedido1)
+order1.change_status(OrderStatus.DELIVERED)
+print(order1)
 
-pedido1.alterar_status(StatusPedido.ENTREGUE)
-print(pedido1)
-
-pedido2 = Pedido(id=102)
-pedido2.alterar_status(StatusPedido.CANCELADO)
-print(pedido2)
+order2 = Order(order_id=102)
+order2.change_status(OrderStatus.CANCELED)
+print(order2)
